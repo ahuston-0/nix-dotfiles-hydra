066a9f134e620a108b97bfbffe2328913e0c0bc3
Richie Cahill <richie@tmmworkshop.com>
2023-12-29 13:32:20 -0500

    added jeeves-jr constructSystem and richie user

---
+++ users/richie/home.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+{
+  home.username = "richie";
+  home.homeDirectory = "/home/richie";
+
+  home.packages = with pkgs; [
+    # Rust packages
+    topgrade
+    trunk
+    wasm-pack
+    cargo-watch
+    #pkgs.cargo-tarpaulin
+    cargo-generate
+    cargo-audit
+    cargo-update
+    diesel-cli
+    # gitoxide currently broke 09182023
+    gitoxide
+    tealdeer
+    helix
+
+    # nix specific packages
+    nil
+    nixfmt
+
+    # markdown
+    nodePackages.markdownlint-cli
+
+    # doom emacs dependencies
+    fd
+    ripgrep
+    clang
+  ];
+
+  programs.zsh.enable = true;
+
+  home.stateVersion = "23.11";
+}
