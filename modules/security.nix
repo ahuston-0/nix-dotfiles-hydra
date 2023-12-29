70e69fc869e23ec924865407885f2dad7785e622
Dennis Wuitz <dennish@wuitz.de>
2023-12-29 14:53:19 +0100

    fix vaultwarden

---
+++ modules/security.nix
@@ -0,0 +1,21 @@
+{ config, lib, ... }:
+{
+  config = {
+    services = lib.mkIf config.services.gitea.enable {
+      openssh = {
+        extraConfig = ''
+          Match User gitea
+            AllowAgentForwarding no
+            AllowTcpForwarding no
+            PermitTTY no
+            X11Forwarding no
+        '';
+      };
+
+      gitea.settings."ssh.minimum_key_sizes" = {
+        ECDSA = -1;
+        RSA = 4095;
+      };
+    };
+  };
+}
