66fca9da44bc67ae5c7e1c9d8905117c7fb7b604
Richie Cahill <richie@tmmworkshop.com>
2023-12-29 13:23:05 -0500

    made configuration.nix for jeeves-jr

---
+++ systems/jeeves-jr/configuration.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+{
+  time.timeZone = "America/New_York";
+  console.keyMap = "us";
+  networking.hostId = "1beb3026";
+
+  boot = {
+    zfs.extraPools = [ "ZFS-primary" ];
+    filesystem = "zfs";
+    useSystemdBoot = true;
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      recommendedDefaults = true;
+      logDriver = "local";
+      daemon."settings" = {
+        experimental = true;
+        exec-opts = [ "native.cgroupdriver=systemd" ];
+        log-opts = {
+          max-size = "10m";
+          max-file = "5";
+        };
+        data-root = "/var/lib/docker";
+      };
+      storageDriver = "overlay2";
+    };
+
+    podman = {
+      enable = true;
+      recommendedDefaults = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+
+  services = {
+    nfs.server.enable = true;
+
+    openssh.ports = [ 352 ];
+    smartd.enable = true;
+    zfs = {
+      trim.enable = true;
+      autoScrub.enable = true;
+    };
+  };
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "23.05";
+}
