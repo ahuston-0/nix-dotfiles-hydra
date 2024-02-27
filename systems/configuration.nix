c35ee793a10b35e2bd0583d1d5784791de7812be
ahuston-0 <aliceghuston@gmail.com>
2024-02-27 03:00:32 -0500

    add space optimizations for servers
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/configuration.nix
+++ systems/configuration.nix
@@ -187,9 +187,10 @@
       connect-timeout = 20;
     };
 
+    # free up to 10 gb when only 1 gb left
     extraOptions = ''
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
+      min-free = ${toString (1 * 1024 * 1024 * 1024)}
+      max-free = ${toString (10 * 1024 * 1024 * 1024)}
     '';
 
     gc = {
