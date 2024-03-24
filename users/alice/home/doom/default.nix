26b8b000435442f37451fa68c3e126cbca97112c
ahuston-0 <aliceghuston@gmail.com>
2024-03-24 15:49:42 -0400

    fix doom path
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/home/doom/default.nix
+++ users/alice/home/doom/default.nix
@@ -6,10 +6,10 @@
 }:
 
 {
-  xdg.configFile."doom/config.el".source = "./doom/config.el";
-  xdg.configFile."doom/custom.el".source = "./doom/custom.el";
-  xdg.configFile."doom/init.el".source = "./doom/init.el";
-  xdg.configFile."doom/packages.el".source = "./doom/packages.el";
-  xdg.configFile."doom/snippets/cc-mode/cc-doxy".source = "./doom/snippets/cc-mode/cc-doxy";
-  xdg.configFile."doom/snippets/cc-mode/README.md".source = "./doom/snippets/cc-mode/README.md";
+  xdg.configFile."doom/config.el".source = ./config.el;
+  xdg.configFile."doom/custom.el".source = ./custom.el;
+  xdg.configFile."doom/init.el".source = ./init.el;
+  xdg.configFile."doom/packages.el".source = ./packages.el;
+  xdg.configFile."doom/snippets/cc-mode/cc-doxy".source = ./snippets/cc-mode/cc-doxy;
+  xdg.configFile."doom/snippets/cc-mode/README.md".source = ./snippets/cc-mode/README.md;
 }
