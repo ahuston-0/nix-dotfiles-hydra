c47f0b4fe9df7ba08c80912964d9f572636e55cb
ahuston-0 <aliceghuston@gmail.com>
2024-04-17 20:51:36 -0400

    hydra updates!
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- flake.nix
+++ flake.nix
@@ -308,14 +308,12 @@
           packages = [
             self.formatter.${system}
             nixpkgs.legacyPackages.${system}.deadnix
-<<<<<<< HEAD
             nixpkgs.legacyPackages.${system}.treefmt
             nixpkgs.legacyPackages.${system}.pre-commit
-=======
->>>>>>> 7502153 (fixes :))
           ];
           shellHook = (nix-pre-commit.lib.${system}.mkConfig { inherit pkgs config; }).shellHook;
         }
       ) sops-nix.packages;
     };
+
 }
---
+++ hydra.nix
@@ -0,0 +1,9 @@
+{ inputs, outputs }:
+let
+  inherit (inputs.nixpkgs.lib) mapAttrs;
+
+  getCfg = _: cfg: cfg.config.system.build.toplevel;
+in
+{
+  hosts = mapAttrs getCfg outputs.nixosConfigurations;
+}
