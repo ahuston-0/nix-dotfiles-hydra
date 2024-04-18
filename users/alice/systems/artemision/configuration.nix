7cc9fb926a5b042c5d26009c2e0e36c16d57bdbe
ahuston-0 <aliceghuston@gmail.com>
2024-04-17 20:50:57 -0400

    fixes :)
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/systems/artemision/configuration.nix
+++ users/alice/systems/artemision/configuration.nix
@@ -37,11 +37,11 @@
 
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
-  #  services.fwupd.package =
-  #    (import (builtins.fetchTarball {
-  #      url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
-  #      sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
-  #    }) { inherit (pkgs) system; }).fwupd;
+  services.fwupd.package =
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+      sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+    }) { inherit (pkgs) system; }).fwupd;
 
   services.fprintd.enable = true;
 
