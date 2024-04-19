a699f53e27e569635da138d623f9f00208e3548f
ahuston-0 <aliceghuston@gmail.com>
2024-04-19 14:09:20 -0400

    remove pre-commit-update hook, re-enable formatter hook
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/home/zsh.nix
+++ users/alice/home/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkg, ... }:
 {
 
   programs.zsh = {
@@ -20,6 +20,18 @@
         #"fzf"
       ];
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkg.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
     initExtra = ''
       # functions
       function mount-data {
