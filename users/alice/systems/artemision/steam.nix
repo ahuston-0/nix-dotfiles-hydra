8885cae07efc58ea6e8c82297ca032e902734472
ahuston-0 <aliceghuston@gmail.com>
2024-05-06 10:37:55 -0400

    enable steam-run, gamescope
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/systems/artemision/steam.nix
+++ users/alice/systems/artemision/steam.nix
@@ -6,11 +6,17 @@
 }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
-    extest.enable = true;
+  environment.systemPackages = [ pkgs.steam-run ];
+  hardware.steam-hardware.enable = true;
+  programs = {
+    gamescope.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+      gamescopeSession.enable = true;
+      extest.enable = true;
+    };
   };
 }
