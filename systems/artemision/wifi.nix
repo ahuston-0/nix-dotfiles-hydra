0d6c05f7081517a93cbed27d5222910880fe615d
ahuston-0 <aliceghuston@gmail.com>
2024-05-19 13:05:28 -0400

    wifi fixes
    artemision is currently having some really weird issues whenever its
    using the local unbound DNS, so this is demoting the local DNS and
    adding some public networks
    
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/artemision/wifi.nix
+++ systems/artemision/wifi.nix
@@ -1,22 +1,36 @@
 { config, ... }:
+let
+  always = 100;
+  home = 99;
+in
 {
   networking.wireless = {
     enable = true;
     environmentFile = config.sops.secrets."wifi-env".path;
+    userControlled.enable = true;
     networks = {
-      "taetaethegae-2.0".psk = "@PASS_taetaethegae_20@";
-      "k".psk = "@PASS_k@";
+      "taetaethegae-2.0" = {
+        psk = "@PASS_taetaethegae_20@";
+        priority = home;
+      };
+      "k" = {
+        psk = "@PASS_k@";
+        priority = always;
+      };
       "Bloomfield".psk = "@PASS_bloomfield@";
       "9872441500".psk = "@PASS_longboat_home@";
       "9872441561".psk = "@PASS_longboat_home@";
       "5HuFios".psk = "@PASS_longboat_home@";
       "24HuFios".psk = "@PASS_longboat_home@";
+      "optimumwifi" = { };
+      "CableWiFi" = { };
     };
   };
 
   networking.nameservers = [
-    "192.168.76.1"
     "9.9.9.9"
+    "1.1.1.1"
+    "192.168.76.1"
   ];
 
   services.resolved = {
