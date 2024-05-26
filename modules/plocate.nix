41dde8c111f7f348d5576af01c9a81f19be9f806
ahuston-0 <aliceghuston@gmail.com>
2024-05-26 19:12:23 -0400

    fix locate package
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- modules/plocate.nix
+++ modules/plocate.nix
@@ -1,6 +1,8 @@
 { lib, pkgs, ... }:
 
 {
-  services.locate.enable = lib.mkDefault true;
-  environment.systemPackages = [ pkgs.plocate ];
+  services.locate = {
+    enable = lib.mkDefault true;
+    package = lib.mkDefault pkgs.plocate;
+  };
 }
