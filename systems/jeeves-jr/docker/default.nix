86ccb18ab46ad204eb5c7d9c9e6893187c26ce8c
Richie Cahill <Richie@tmmworkshop.com>
2024-05-27 20:26:30 -0400

    removed activationScripts.mkVPN

--- systems/jeeves-jr/docker/default.nix
+++ systems/jeeves-jr/docker/default.nix
@@ -1,15 +1,5 @@
-{ pkgs, config, ... }:
 {
   imports = [ ./web.nix ];
 
   virtualisation.oci-containers.backend = "docker";
-
-  system.activationScripts.mkVPN =
-    let
-      docker = config.virtualisation.oci-containers.backend;
-      dockerBin = "${pkgs.${docker}}/bin/${docker}";
-    in
-    ''
-      ${dockerBin} network inspect web >/dev/null 2>&1 || ${dockerBin} network create web --subnet 172.100.5.0/16
-    '';
 }
