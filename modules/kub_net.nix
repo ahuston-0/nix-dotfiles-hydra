961d262f8f0278c4190468b1fd83122e864fce5f
ahuston-0 <aliceghuston@gmail.com>
2024-05-28 23:41:22 -0400

    add a toggle to kub_net, as we are getting warnings currently
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- modules/kub_net.nix
+++ modules/kub_net.nix
@@ -1,39 +1,53 @@
 { lib, ... }:
 {
-  system.activationScripts.setZerotierName = lib.stringAfter [ "var" ] ''
-    echo "ebe7fbd44565ba9d=ztkubnet" > /var/lib/zerotier-one/devicemap 
-  '';
-
-  services.zerotierone = {
-    enable = true;
-    joinNetworks = [ "ebe7fbd44565ba9d" ];
+  options = {
+    services.rad-dev.k3s-net = {
+      enable = lib.mkOption {
+        default = true;
+        example = true;
+        description = "Whether to enable k3s-net.";
+        type = lib.types.bool;
+      };
+    };
   };
 
-  systemd.network = {
-    enable = true;
-    wait-online.anyInterface = true;
-    netdevs = {
-      "20-brkubnet" = {
-        netdevConfig = {
-          Kind = "bridge";
-          Name = "brkubnet";
+  config = lib.mkIf {
+
+    system.activationScripts.setZerotierName = lib.stringAfter [ "var" ] ''
+      echo "ebe7fbd44565ba9d=ztkubnet" > /var/lib/zerotier-one/devicemap 
+    '';
+
+    services.zerotierone = {
+      enable = true;
+      joinNetworks = [ "ebe7fbd44565ba9d" ];
+    };
+
+    systemd.network = {
+      enable = true;
+      wait-online.anyInterface = true;
+      netdevs = {
+        "20-brkubnet" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "brkubnet";
+          };
+        };
+      };
+      networks = {
+        "30-ztkubnet" = {
+          matchConfig.Name = "ztkubnet";
+          networkConfig.Bridge = "brkubnet";
+          linkConfig.RequiredForOnline = "enslaved";
+        };
+        "40-brkubnet" = {
+          matchConfig.Name = "brkubnet";
+          bridgeConfig = { };
+          linkConfig.RequiredForOnline = "no";
         };
       };
     };
-    networks = {
-      "30-ztkubnet" = {
-        matchConfig.Name = "ztkubnet";
-        networkConfig.Bridge = "brkubnet";
-        linkConfig.RequiredForOnline = "enslaved";
-      };
-      "40-brkubnet" = {
-        matchConfig.Name = "brkubnet";
-        bridgeConfig = { };
-        linkConfig.RequiredForOnline = "no";
-      };
-    };
-  };
 
-  # enable experimental networkd backend so networking doesnt break on hybrid systems
-  networking.useNetworkd = true;
+    # enable experimental networkd backend so networking doesnt break on hybrid systems
+    networking.useNetworkd = true;
+  };
 }
