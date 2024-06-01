ac072ce425d8c26abe7c53b1ec2e7c0d75cc6df1
Richie Cahill <Richie@tmmworkshop.com>
2024-06-01 10:51:37 -0400

    added bob to sshconfig

--- users/richie/home/sshconfig.nix
+++ users/richie/home/sshconfig.nix
@@ -15,6 +15,12 @@
         identityFile = "~/.ssh/id_ed25519";
         port = 352;
       };
+      bob = {
+        hostname = "192.168.90.25";
+        user = "richie";
+        identityFile = "~/.ssh/id_ed25519";
+        port = 262;
+      };
     };
   };
 }
