b35dd7017a44f88b2d84075065eb7a6a38d84970
Richie Cahill <Richie@tmmworkshop.com>
2024-06-14 11:11:55 -0400

    removed finegrained power management

--- systems/bob/nvidia.nix
+++ systems/bob/nvidia.nix
@@ -4,10 +4,7 @@
   hardware = {
     nvidia = {
       modesetting.enable = true;
-      powerManagement = {
-        enable = true;
-        finegrained = true;
-      };
+      powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.production;
       nvidiaSettings = true;
     };
