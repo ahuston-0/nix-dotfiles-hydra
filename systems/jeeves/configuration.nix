b1e79d14fc783676edbdb8b5f04ee1925e0e12aa
Richie Cahill <Richie@tmmworkshop.com>
2024-06-20 22:27:14 -0400

    moved scripts

--- systems/jeeves/configuration.nix
+++ systems/jeeves/configuration.nix
@@ -131,7 +131,7 @@
       serviceConfig = {
         Environment = "ZFS_BIN=${pkgs.zfs}/bin/zfs";
         Type = "oneshot";
-        ExecStart = "${pkgs.python3}/bin/python3 /zfs/media/Scripts/new/zfs/snapshot_manager.py --config-file='/root/nix-dotfiles/systems/jeeves/snapshot_config.toml'";
+        ExecStart = "${pkgs.python3}/bin/python3 /zfs/media/scripts/new/zfs/snapshot_manager.py --config-file='/root/nix-dotfiles/systems/jeeves/snapshot_config.toml'";
       };
     };
     timers."snapshot_manager" = {
