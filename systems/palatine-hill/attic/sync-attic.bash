95983b77b95ea592330d2456af785efe8b4c7476
ahuston-0 <aliceghuston@gmail.com>
2024-06-22 12:58:40 -0400

    fix sync directories
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- systems/palatine-hill/attic/sync-attic.bash
+++ systems/palatine-hill/attic/sync-attic.bash
@@ -1,4 +1,10 @@
 #!/usr/bin/env nix
 #! nix shell nixpkgs#bash nixpkgs#findutils nixpkgs#attic-client --command bash
 
-find . -regex ".*\.drv$" -exec attic push cache-nix-dot '/ZFS/ZFS-primary/hydra/{}' \;
+sync_directories=(
+    /ZFS/ZFS-primary/hydra
+)
+
+for dir in "${sync_directories[@]}"; do
+    find "$dir"  -regex ".*\.drv$" -exec attic push cache-nix-dot '{}' \;
+done
