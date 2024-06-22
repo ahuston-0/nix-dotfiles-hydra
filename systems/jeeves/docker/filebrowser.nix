1603e2ee64cafd5f08a2b8c019d3994770e52c9f
Richie Cahill <Richie@tmmworkshop.com>
2024-06-22 13:10:33 -0400

    adding ports for testing

--- systems/jeeves/docker/filebrowser.nix
+++ systems/jeeves/docker/filebrowser.nix
@@ -2,6 +2,7 @@
   virtualisation.oci-containers.containers.filebrowser = {
     image = "hurlenko/filebrowser";
     extraOptions = [ "--network=web" ];
+    ports = [ "8080:8080" ];
     volumes = [
       "/ZFS:/data"
       "/zfs/media/docker/configs/filebrowser:/config"
