15ef80722b6d34bf3fc966648c81205bd80c7969
Richie Cahill <Richie@tmmworkshop.com>
2024-06-22 13:42:12 -0400

    removed ports for testing

--- systems/jeeves/docker/filebrowser.nix
+++ systems/jeeves/docker/filebrowser.nix
@@ -2,7 +2,6 @@
   virtualisation.oci-containers.containers.filebrowser = {
     image = "hurlenko/filebrowser";
     extraOptions = [ "--network=web" ];
-    ports = [ "8080:8080" ];
     volumes = [
       "/ZFS:/data"
       "/zfs/media/docker/configs/filebrowser:/config"
