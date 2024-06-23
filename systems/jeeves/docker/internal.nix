81256cfd22445f93d3eef7b70f69a83221247f6f
Richie Cahill <Richie@tmmworkshop.com>
2024-06-23 11:54:37 -0400

    fixing name server

--- systems/jeeves/docker/internal.nix
+++ systems/jeeves/docker/internal.nix
@@ -44,7 +44,7 @@
         STRICT_PORT_FORWARD = "yes";
         ENABLE_PRIVOXY = "yes";
         LAN_NETWORK = "192.168.90.0/24";
-        NAME_SERVERS = "192.168.90.0";
+        NAME_SERVERS = "192.168.99.1";
         UMASK = "000";
         DEBUG = "false";
         DELUGE_DAEMON_LOG_LEVEL = "debug";
