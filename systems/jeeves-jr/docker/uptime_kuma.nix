0ff0c926c47ae9b0edf2b3c8158c61b67329523b
Richie Cahill <Richie@tmmworkshop.com>
2024-07-05 14:40:02 -0400

    fixed typo

--- systems/jeeves-jr/docker/uptime_kuma.nix
+++ systems/jeeves-jr/docker/uptime_kuma.nix
@@ -7,7 +7,7 @@ in
       image = "louislam/uptime-kuma:latest";
       ports = [ "3001:3001" ];
       volumes = [
-        "${vars.media_docker_configs}/uptime_kuma:/app/data"
+        "${vars.main_docker_configs}/uptime_kuma:/app/data"
         "/var/run/docker.sock:/var/run/docker.sock"
       ];
       extraOptions = [ "--network=web" ];
