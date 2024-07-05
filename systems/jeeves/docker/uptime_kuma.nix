a1e5dd05867f0c0ed13f8996bd138aeb7f9dd047
Richie Cahill <Richie@tmmworkshop.com>
2024-07-05 11:56:42 -0400

    added docker sock

--- systems/jeeves/docker/uptime_kuma.nix
+++ systems/jeeves/docker/uptime_kuma.nix
@@ -6,7 +6,10 @@ in
     uptime_kuma = {
       image = "louislam/uptime-kuma:latest";
       ports = [ "3001:3001" ];
-      volumes = [ "${vars.media_docker_configs}/uptime_kuma:/app/data" ];
+      volumes = [
+        "${vars.media_docker_configs}/uptime_kuma:/app/data"
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
       extraOptions = [ "--network=web" ];
       autoStart = true;
     };
