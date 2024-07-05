9b9a0f2a735ba752b436da5abb2ba2e1dc8effe4
Richie Cahill <Richie@tmmworkshop.com>
2024-07-05 13:23:43 -0400

    updating jeevesjr docker default.nix

--- systems/jeeves-jr/docker/default.nix
+++ systems/jeeves-jr/docker/default.nix
@@ -1,5 +1,11 @@
+{ lib, ... }:
 {
-  imports = [ ./web.nix ];
+  imports =
+    let
+      files = builtins.attrNames (builtins.readDir ./.);
+      nixFiles = builtins.filter (name: lib.hasSuffix ".nix" name && name != "default.nix") files;
+    in
+    map (file: ./. + "/${file}") nixFiles;
 
   virtualisation.oci-containers.backend = "docker";
 }
