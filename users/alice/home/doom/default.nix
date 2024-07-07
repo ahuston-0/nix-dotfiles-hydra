61c38bba3c03da4fa97c14d95ebcb6cc0287586b
ahuston-0 <aliceghuston@gmail.com>
2024-07-07 19:56:49 -0400

    remove cc-doxy
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- users/alice/home/doom/default.nix
+++ users/alice/home/doom/default.nix
@@ -6,7 +6,5 @@
     "doom/custom.el".source = ./custom.el;
     "doom/init.el".source = ./init.el;
     "doom/packages.el".source = ./packages.el;
-    "doom/snippets/cc-mode/cc-doxy".source = ./snippets/cc-mode/cc-doxy;
-    "doom/snippets/cc-mode/README.md".source = ./snippets/cc-mode/README.md;
   };
 }
