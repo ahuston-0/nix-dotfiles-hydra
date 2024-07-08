d1e0695efedb21e1bce8ab06d2a7dece293cfd8c
ahuston-0 <aliceghuston@gmail.com>
2024-07-07 20:52:04 -0400

    make hosts an aggregate job
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- hydra/jobs.nix
+++ hydra/jobs.nix
@@ -1,10 +1,14 @@
 { inputs, outputs }:
 let
   inherit (inputs.nixpkgs.lib) mapAttrs;
+  pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 
   getCfg = _: cfg: cfg.config.system.build.toplevel;
 in
 {
-  inherit (outputs) formatter devShells;
-  hosts = mapAttrs getCfg outputs.nixosConfigurations;
+  inherit (outputs) formatter devShells checks;
+  hosts = pkgs.releaseTools.aggregate {
+    name = "hosts";
+    constituents = mapAttrs getCfg outputs.nixosConfigurations;
+  };
 }
