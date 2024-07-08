0b1d843fd2cad0fa0368618785d30e6e557cbf85
ahuston-0 <aliceghuston@gmail.com>
2024-07-07 20:59:44 -0400

    mapAttrs -> mapAttrsToList for hydra aggregate
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- hydra/jobs.nix
+++ hydra/jobs.nix
@@ -1,6 +1,6 @@
 { inputs, outputs }:
 let
-  inherit (inputs.nixpkgs.lib) mapAttrs;
+  inherit (inputs.nixpkgs.lib) mapAttrsToList;
   pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 
   getCfg = _: cfg: cfg.config.system.build.toplevel;
@@ -9,6 +9,6 @@ in
   inherit (outputs) formatter devShells checks;
   hosts = pkgs.releaseTools.aggregate {
     name = "hosts";
-    constituents = mapAttrs getCfg outputs.nixosConfigurations;
+    constituents = mapAttrsToList getCfg outputs.nixosConfigurations;
   };
 }
